a76ffed5c9d81cb91f989a79f29c254b358a16c7
HelloGbyeEvan <evan.chow@hellogbye.com>
2019-04-02 15:42:18 -0400

    changed indentation to 2-spaces - EC

--- src/script.js
+++ src/script.js
@@ -1,21 +1,20 @@
-
 var sheetData = undefined;
 var groupGlobal = undefined;
 var xGlobal = undefined;
 var yGlobal = undefined;
 
 
-$(document).ready(function() {
+$(document).ready(function () {
+  getData();
+  $("#data").click(function () {
     getData();
-  $("#data").click(function(){
-      getData();
   });
-  $("#test").click(function(){
+  $("#test").click(function () {
     convertCyclesToDays(sheetData.cycles);
     d3.selectAll("svg > *").remove();
-    drawGraph(sheetData.cycles,"name","date","read");
+    drawGraph(sheetData.cycles, "name", "date", "read");
   });
-  $("#graph").click(function(){
+  $("#graph").click(function () {
     d3.selectAll("svg > *").remove();
     var groupForm = document.getElementById("group");
     var xForm = document.getElementById("x-axis");
@@ -29,191 +28,196 @@ $(document).ready(function() {
 });
 
 
-class Day{
-  constructor(cycles){
-      this.date = new Date(cycles[0].date);
-      this.time = this.date.getTime();
-      this.read = cycles.map(cycle => cycle.read).reduce(getSum);
-      this.norm = cycles.map(cycle => cycle.norm).reduce(getSum);
-      this.correct = cycles.map(cycle => cycle.correct).reduce(getSum);
-      this.suspense = cycles.map(cycle => cycle.suspense).reduce(getSum);
-      this.easy = cycles.map(cycle => cycle.easy).reduce(getSum);
-      this.med = cycles.map(cycle => cycle.med).reduce(getSum);
-      this.com = cycles.map(cycle => cycle.com).reduce(getSum);
+class Day {
+  constructor(cycles) {
+    this.date = new Date(cycles[0].date);
+    this.time = this.date.getTime();
+    this.read = cycles.map(cycle => cycle.read).reduce(getSum);
+    this.norm = cycles.map(cycle => cycle.norm).reduce(getSum);
+    this.correct = cycles.map(cycle => cycle.correct).reduce(getSum);
+    this.suspense = cycles.map(cycle => cycle.suspense).reduce(getSum);
+    this.easy = cycles.map(cycle => cycle.easy).reduce(getSum);
+    this.med = cycles.map(cycle => cycle.med).reduce(getSum);
+    this.com = cycles.map(cycle => cycle.com).reduce(getSum);
   }
 }
 
-function getSum(total, num){
-    return total+num;
-}
-function convertCyclesToDays(cycles){
-    var test = restructureData(cycles,"date").map(o=>new Day(o.values));
+function getSum(total, num) {
+  return total + num;
 }
 
-function restructureData(data, key){
-    var dataGroup = d3.nest()
-        .key(function(d) {
-            return d[key];
-        })
-        .entries(data);
-    if(key==="date"){
-        dataGroup = [{
-            key: "Date",
-            values: dataGroup.map(o=>new Day(o.values))
-        }]
-    }
-    console.log(dataGroup)
-    return dataGroup;
+function convertCyclesToDays(cycles) {
+  var test = restructureData(cycles, "date").map(o => new Day(o.values));
 }
 
-function getTitle(label){
-    switch(label){
-        case "date": return "Date";
-        case "cycles": return "Cycle";
-        case "read": return "Emails Read";
-        case "norm": return "Emails Normalized";
-        case "easy": return "Easy Emails Normalized";
-        case "med": return "Medium Emails Normalized";
-        case "com": return "Complex Emails Normalized";
-        case "suspense": return "Emails Suspensed";
-        case "correct": return "Emails Correct";
-        default: return "ERR: INCORRECT VAR GIVEN";
-    }
+function restructureData(data, key) {
+  var dataGroup = d3.nest()
+    .key(function (d) {
+      return d[key];
+    })
+    .entries(data);
+  if (key === "date") {
+    dataGroup = [{
+      key: "Date",
+      values: dataGroup.map(o => new Day(o.values))
+    }]
+  }
+  console.log(dataGroup)
+  return dataGroup;
 }
 
-function getData(){
-    $.get("http://localhost:8000/api/getdata", function(data, err){
-      sheetData = data;
-    });
+function getTitle(label) {
+  switch (label) {
+    case "date": return "Date";
+    case "cycles": return "Cycle";
+    case "read": return "Emails Read";
+    case "norm": return "Emails Normalized";
+    case "easy": return "Easy Emails Normalized";
+    case "med": return "Medium Emails Normalized";
+    case "com": return "Complex Emails Normalized";
+    case "suspense": return "Emails Suspensed";
+    case "correct": return "Emails Correct";
+    default: return "ERR: INCORRECT VAR GIVEN";
+  }
+}
+
+function getData() {
+  $.get("http://localhost:8000/api/getdata", function (data, err) {
+    sheetData = data;
+  });
 }
 
 function drawGraph(data, key, xVar, yVar) {
-    var dataGroup = restructureData(data,key);
-    var xTitle = getTitle(xVar);
-    var yTitle = getTitle(yVar);
-    var vis = d3.select("#visualization"),
+  var dataGroup = restructureData(data, key);
+  var xTitle = getTitle(xVar);
+  var yTitle = getTitle(yVar);
+  var vis = d3.select("#visualization"),
     w = window,
     d = document,
     e = d.documentElement,
     g = d.getElementsByTagName('body')[0],
     plot = d3.select("#plot")
-    console.log(plot);
-    WIDTH = (plot.clientWidth || e.clientWidth || w.innerWidth || g.clientWidth)*(8/10),
-    HEIGHT =(plot.clientHeight || e.clientHeight || w.innerHeight || g.clientHeight)*(8/10),
+  console.log(plot);
+  WIDTH = (plot.clientWidth || e.clientWidth || w.innerWidth || g.clientWidth) * (8 / 10),
+    HEIGHT = (plot.clientHeight || e.clientHeight || w.innerHeight || g.clientHeight) * (8 / 10),
     MARGINS = {
-        top: 50,
-        right: 100,
-        bottom: 40,
-        left: 60,
+      top: 50,
+      right: 100,
+      bottom: 40,
+      left: 60,
     },
-    xScale = xVar==="date" ? d3.scaleTime().range([MARGINS.left, WIDTH-MARGINS.right]).domain([d3.min(data, function(d){
-        return d.time;
-    }), d3.max(data, function(d){
-        return d.time;
-    })]) : d3.scaleLinear().range([MARGINS.left, WIDTH-MARGINS.right]).domain([d3.min(data, function(d,i) {
-        return 0;
-    }), d3.max(dataGroup, function(d,i) {
-        return d.values.length;
+    xScale = xVar === "date" ? d3.scaleTime().range([MARGINS.left, WIDTH - MARGINS.right]).domain([d3.min(data, function (d) {
+      return d.time;
+    }), d3.max(data, function (d) {
+      return d.time;
+    })]) : d3.scaleLinear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([d3.min(data, function (d, i) {
+      return 0;
+    }), d3.max(dataGroup, function (d, i) {
+      return d.values.length;
     })]),
-    yScale = key!=="date" ? d3.scaleLinear().range([HEIGHT - MARGINS.top, 10]).domain([d3.max([0,d3.min(data,function(d){
-        return d[yVar];
-    })-10]), d3.max(data, function(d) {
-        return d[yVar];
-    })]) : d3.scaleLinear().range([HEIGHT-MARGINS.top,10]).domain([d3.min(dataGroup[0].values,function(d){
-        return d[yVar]
-    }), d3.max(dataGroup[0].values, function(d){
-        return d[yVar]
+    yScale = key !== "date" ? d3.scaleLinear().range([HEIGHT - MARGINS.top, 10]).domain([d3.max([0, d3.min(data, function (d) {
+      return d[yVar];
+    }) - 10]), d3.max(data, function (d) {
+      return d[yVar];
+    })]) : d3.scaleLinear().range([HEIGHT - MARGINS.top, 10]).domain([d3.min(dataGroup[0].values, function (d) {
+      return d[yVar]
+    }), d3.max(dataGroup[0].values, function (d) {
+      return d[yVar]
     })]),
     xAxis = d3.axisBottom()
-        .scale(xScale),
+    .scale(xScale),
 
     yAxis = d3.axisLeft()
-        .scale(yScale);
+    .scale(yScale);
 
-    vis.append("svg:g")
-        .attr("class","axis")
-        .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom - 10) + ")")
-        .call(xAxis);
-        
-    vis.append("svg:g")
-        .attr("class","axis")
-        .attr("transform", "translate(" + (MARGINS.left) + ",0)")
-        .call(yAxis);
+  vis.append("svg:g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom - 10) + ")")
+    .call(xAxis);
 
-    vis.append("text")             
-        .attr("transform",
-            "translate(" + (WIDTH/2) + " ," + 
-            (HEIGHT + MARGINS.top - 50) + ")")
-        .style("text-anchor", "middle")
-        .style("font-weight", "bold")
-        .text(xTitle);
+  vis.append("svg:g")
+    .attr("class", "axis")
+    .attr("transform", "translate(" + (MARGINS.left) + ",0)")
+    .call(yAxis);
 
-    vis.append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", MARGINS.left - 60)
-        .attr("x",0 - (HEIGHT/2))
-        .attr("dy", "1em")
-        .style("text-anchor", "middle")
-        .style("font-weight", "bold")
-        .text(yTitle);
+  vis.append("text")
+    .attr("transform",
+      "translate(" + (WIDTH / 2) + " ," +
+      (HEIGHT + MARGINS.top - 50) + ")")
+    .style("text-anchor", "middle")
+    .style("font-weight", "bold")
+    .text(xTitle);
 
-    var lineGen = d3.line()
-        .x(function(d, i) {
-            return xVar==="date"?xScale(d.time):xScale(i);
-        })
-        .y(function(d) {
-            return yScale(d[yVar]);
-        })
-        .curve(d3.curveMonotoneX);
+  vis.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", MARGINS.left - 60)
+    .attr("x", 0 - (HEIGHT / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .style("font-weight", "bold")
+    .text(yTitle);
 
-    var colors = new Array("hsl(0, 100%, 50%", "hsl(40, 100%, 50%)", "hsl(120, 100%, 50%)",
+  var lineGen = d3.line()
+    .x(function (d, i) {
+      return xVar === "date" ? xScale(d.time) : xScale(i);
+    })
+    .y(function (d) {
+      return yScale(d[yVar]);
+    })
+    .curve(d3.curveMonotoneX);
+
+  var colors = new Array("hsl(0, 100%, 50%", "hsl(40, 100%, 50%)", "hsl(120, 100%, 50%)",
     "hsl(180, 100%, 50%)", "hsl(240, 100%, 50%)", "hsl(300, 100%, 50%)");
-    dataGroup.forEach(function(d, i) {
-        var colored = colors[i];
-        vis.append('svg:path')
-            .attr('d', lineGen(d.values))
-            .attr('stroke', colored)
-            .attr('stroke-width', 2)
-            .attr('id', 'line_'+d.key)
-            .attr('fill', 'none');
-        vis.selectAll("dot")
-            .data(d.values)
-        .enter().append("circle")
-            .style("fill", colored)
-            .attr('id', 'value_'+d.key)
-            .attr("r", 5)
-            .attr('id', 'value_' +d.key)
-            .attr("cx", function(d, i) { return xVar==="date"?xScale(d.time):xScale(i); })
-            .attr("cy", function(d) { return yScale(d[yVar]); });
-        lSpace = HEIGHT/dataGroup.length;
-        vis.append("text")
-            .attr("x", WIDTH - 40)
-            .attr("y", (lSpace / 2) + i * lSpace)
-            .style("fill", "black")
-            .attr("class", "legend")
-            .text(d.key)
-            .on('click', function() {
-                var active = d.active ? false : true;
-                var opacity = active ? 0 : 1;
-            
-                d3.select("#line_" + d.key).style("opacity", opacity);
-            
-                d.active = active;
-                d3.selectAll("#value_" + d.key).style("opacity", opacity);
-                
-            });
-        var circle = vis.append("circle")
-            .style("fill", colored)
-            .attr("cx", WIDTH - 55)
-            .attr("cy", ((lSpace / 2) + i * lSpace) - 5)
-            .attr("r", 7)
-            .on('click', function() {
-                var active = d.active ? false : true;
-                var opacity = active ? 0 : 1;
-            
-                d3.select("#value_" + d.key).style("opacity", opacity);
-            
-                d.active = active;
-            });
-    });
+  dataGroup.forEach(function (d, i) {
+    var colored = colors[i];
+    vis.append('svg:path')
+      .attr('d', lineGen(d.values))
+      .attr('stroke', colored)
+      .attr('stroke-width', 2)
+      .attr('id', 'line_' + d.key)
+      .attr('fill', 'none');
+    vis.selectAll("dot")
+      .data(d.values)
+      .enter().append("circle")
+      .style("fill", colored)
+      .attr('id', 'value_' + d.key)
+      .attr("r", 5)
+      .attr('id', 'value_' + d.key)
+      .attr("cx", function (d, i) {
+        return xVar === "date" ? xScale(d.time) : xScale(i);
+      })
+      .attr("cy", function (d) {
+        return yScale(d[yVar]);
+      });
+    lSpace = HEIGHT / dataGroup.length;
+    vis.append("text")
+      .attr("x", WIDTH - 40)
+      .attr("y", (lSpace / 2) + i * lSpace)
+      .style("fill", "black")
+      .attr("class", "legend")
+      .text(d.key)
+      .on('click', function () {
+        var active = d.active ? false : true;
+        var opacity = active ? 0 : 1;
+
+        d3.select("#line_" + d.key).style("opacity", opacity);
+
+        d.active = active;
+        d3.selectAll("#value_" + d.key).style("opacity", opacity);
+
+      });
+    var circle = vis.append("circle")
+      .style("fill", colored)
+      .attr("cx", WIDTH - 55)
+      .attr("cy", ((lSpace / 2) + i * lSpace) - 5)
+      .attr("r", 7)
+      .on('click', function () {
+        var active = d.active ? false : true;
+        var opacity = active ? 0 : 1;
+
+        d3.select("#value_" + d.key).style("opacity", opacity);
+
+        d.active = active;
+      });
+  });
 }
