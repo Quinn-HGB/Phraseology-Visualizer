09b6f4b9af2e2024977d6517dba91ac6ddd15495
Alan Xie <alan.xie@hellogbye.com>
2019-04-02 15:42:33 -0400

    spacing

--- server/models/Cycle.js
+++ server/models/Cycle.js
@@ -1,28 +1,24 @@
-
-
 module.exports = class Cycle {
   constructor(cycle) {
-      this.name = cycle[0]
-      this.date = this.dayMonth(cycle);
-      this.time = this.unix(cycle);
-      this.read =     Number(cycle[3]) 
-      this.norm =     Number(cycle[4])
-      this.correct =  Number(cycle[5])
-      this.suspense = Number(cycle[6])
-      this.easy =     Number(cycle[7])
-      this.med =      Number(cycle[8])
-      this.com =      Number(cycle[9])
-      this.easyTime = Number(cycle[10])
+    this.name = cycle[0];
+    this.date = this.dayMonth(cycle);
+    this.time = this.unix(cycle);
+    this.read = Number(cycle[3]);
+    this.norm = Number(cycle[4]);
+    this.correct = Number(cycle[5]);
+    this.suspense = Number(cycle[6]);
+    this.easy = Number(cycle[7]);
+    this.med = Number(cycle[8]);
+    this.com = Number(cycle[9]);
+    this.easyTime = Number(cycle[10]);
   }
-  
-  dayMonth(cycle){
+
+  dayMonth(cycle) {
     var date = new Date(cycle[1] + "T" + "00:00:00");
     return date;
   }
-  unix(cycle){
-    var date = new Date(cycle[1] + "T" +cycle[2]+ ":00");
-    return date.getTime()
+  unix(cycle) {
+    var date = new Date(cycle[1] + "T" + cycle[2] + ":00");
+    return date.getTime();
   }
-}
-
-
+};
--- server/server.js
+++ server/server.js
@@ -1,18 +1,15 @@
-var express = require('express');
-var cors = require('cors');
-var port = process.env.PORT || 8000
-
+var express = require("express");
+var cors = require("cors");
+var port = process.env.PORT || 8000;
 
 var app = express();
 app.use(cors());
 
-app.get('/', function (req, res) {
-  return res.send('Hello! the API is at http:localhost:' + port + '/api')
-})
-var routes = require('./routes');
-app.use('/api', routes);
-
-
+app.get("/", function(req, res) {
+  return res.send("Hello! the API is at http:localhost:" + port + "/api");
+});
+var routes = require("./routes");
+app.use("/api", routes);
 
 app.listen(port);
-console.log('http://localhost:' + port);
+console.log("http://localhost:" + port);
