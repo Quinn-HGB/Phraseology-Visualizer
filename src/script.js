659cb3d9b4a75561fd47192421c7912414956627
HelloGbyeEvan <evan.chow@hellogbye.com>
2019-04-17 16:31:17 -0400

    moved day/dayaverage classes to own sheet -EC

--- src/script.js
+++ src/script.js
@@ -97,57 +97,6 @@ $(document).ready(function() {
      
 
 
-class DayAverage{
-  constructor(cycles) {
-    this.date = new Date(cycles[0].date);
-    this.time = this.date.getTime();
-    this.read = Math.round(cycles.map(cycle => cycle.read).reduce(getSum)/cycles.length*100)/100;
-    this.norm = Math.round(cycles.map(cycle => cycle.norm).reduce(getSum)/cycles.length*100)/100;
-    this.correct = Math.round(cycles.map(cycle => cycle.correct).reduce(getSum)/cycles.length*100)/100;
-    this.suspense = Math.round(cycles.map(cycle => cycle.suspense).reduce(getSum)/cycles.length*100)/100;
-    this.easy = Math.round(cycles.map(cycle => cycle.easy).reduce(getSum)/cycles.length*100)/100;
-    this.med = Math.round(cycles.map(cycle => cycle.med).reduce(getSum)/cycles.length*100)/100;
-    this.com = Math.round(cycles.map(cycle => cycle.com).reduce(getSum)/cycles.length*100)/100;
-    this.easyTime = Math.round(cycles.map(cycle => cycle.easyTime).reduce(getSum)/cycles.length*100)/100;
-    this.medTime = Math.round(cycles.map(cycle => cycle.medTime).reduce(getSum)/cycles.length*100)/100;
-    this.comTime = Math.round(cycles.map(cycle => cycle.comTime).reduce(getSum)/cycles.length*100)/100;
-    this.correctRate = Math.round(this.correct/this.norm*10000)/100;
-    this.easyPercentage = Math.round(this.easy/this.norm*10000)/100;
-    this.medPercentage = Math.round(this.med/this.norm*10000)/100;
-    this.comPercentage = Math.round(this.com/this.norm*10000)/100;
-  }
-}
-
-
-class Day {
-  constructor(cycles) {
-    this.date = new Date(cycles[0].date);
-    this.time = this.date.getTime();
-    this.read = cycles.map(cycle => cycle.read).reduce(getSum);
-    this.norm = cycles.map(cycle => cycle.norm).reduce(getSum);
-    this.correct = cycles.map(cycle => cycle.correct).reduce(getSum);
-    this.suspense = cycles.map(cycle => cycle.suspense).reduce(getSum);
-    this.easy = cycles.map(cycle => cycle.easy).reduce(getSum);
-    this.med = cycles.map(cycle => cycle.med).reduce(getSum);
-    this.com = cycles.map(cycle => cycle.com).reduce(getSum);
-    this.easyTime = cycles.map(cycle => cycle.easyTime).reduce(getSum);
-    this.medTime = cycles.map(cycle => cycle.medTime).reduce(getSum);
-    this.comTime = cycles.map(cycle => cycle.comTime).reduce(getSum);
-    this.correctRate = Math.round(this.correct/this.norm*10000)/100;
-    this.easyPercentage = Math.round(this.easy/this.norm*10000)/100;
-    this.medPercentage = Math.round(this.med/this.norm*10000)/100;
-    this.comPercentage = Math.round(this.com/this.norm*10000)/100;
-  }
-}
-
-function getSum(total, num) {
-  return total + num;
-}
-
-function convertCyclesToDays(cycles) {
-  var test = restructureData(cycles, "date").map(o => new Day(o.values));
-}
-
 function restructureData(data, key, isAverage) {
   var dataGroup = d3.nest()
     .key(function (d) {
